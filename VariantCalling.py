7fad6305cb5b397dde5e13ff9f55ccbf78fbafbe
paopaoch <paopaochofficial@gmail.com>
2023-06-13 23:11:36 +0100

    add Variant Calling Data class

---
+++ VariantCalling.py
@@ -0,0 +1,102 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+class VariantCalling:
+    def __init__(self, mutation_labels, mutation_types_names) -> None:
+        self.mutation_labels = mutation_labels
+        self.mutation_type_names = mutation_types_names
+        self.NUCLEOTIDES = "ACGT"
+        self.transdict = {"A":0, "C": 1, "G":2, "T":3}
+
+class VariantCallingData(VariantCalling):
+    """Class for simulated data generation"""
+    def __init__(self, 
+                 mutation_labels={"no_SNP": 0, "heterozygous_SNP": 1, "homozygous_SNP": 2},
+                 mutation_types_names={0: "No mutation", 1: "Heterozygous SNP", 2: "Homozygous SNP"}
+                 ) -> None:
+        super().__init__(mutation_labels=mutation_labels, mutation_types_names=mutation_types_names)
+        self.alignments = None
+        self.mutation_types = None
+    
+    def simulate_alignments(self, reference_length=200, 
+                        num_alignments = 2000, 
+                        coverage = 100, 
+                        mutations = None,
+                        p_sequencing_error=0.0,
+                        p_alignment_error=0.00) -> tuple:
+        """Modified provided function to generate simulated data for the project.
+        
+        Inputs
+        ------
+        reference_length: Width of the image.
+        num_alignments: Size of data samples (1 sample is 1 image).
+        coverage: Height of the image.
+        mutations: Contains the keys of the mutation types. 
+                    If None, use self.mutation_labels.
+        p_sequencing_error: Probability of sequencing error.
+        p_alignment_error: Probability of alignment error.
+
+        Outputs
+        -------
+        tuple: (alignments, mutation_types)
+        alignments: 3D np.array containing the data in the shape 
+                    (num_alignments, coverage + 1, reference_length).
+        mutation_types: python list of integers indicating the mutation 
+                        type of each sample of data in alignments. 
+                        size of num_alignments.
+        """
+        if mutations is None:
+            mutations = self.mutation_labels.keys()
+
+        alignments = []
+        mutation_types = []
+        
+        for i in range(num_alignments):
+            snp_index = reference_length // 2 
+            if (i % 400 == 0):
+                print("Computing alignment ", i)
+            reference = [random.choice(self.NUCLEOTIDES) for _ in range(reference_length)]
+            reference_base_at_snp = reference[snp_index]
+            snp_base = random.choice([i for i in self.NUCLEOTIDES if i != reference_base_at_snp])
+            mutation_type=random.choice([self.mutation_labels[m] for m in mutations]) 
+            mutation_types.append(mutation_type)
+            
+            alignment = [reference] #first read is always the reference
+            for _ in range(coverage):
+                mut_index = snp_index
+                new_read = [reference[i] if random.random() > p_sequencing_error 
+                            else random.choice(self.NUCLEOTIDES) for i in range(reference_length)]
+                if random.random() < p_alignment_error:
+                    mut_index = snp_index + random.randint(-1,2)
+                if mutation_type == 1 and random.random() > 0.5: # heterozygous SNP
+                    new_read[mut_index] = snp_base            
+                if mutation_type == 2: #homozygous SNP
+                    new_read[mut_index] = snp_base
+                if random.random() < p_sequencing_error: #Add errors to SNP region also
+                    new_read[mut_index] =  random.choice(self.NUCLEOTIDES)
+                alignment.append(new_read)
+            alignments.append(alignment)
+
+        self.alignments = alignments
+        self.mutation_types = mutation_types
+        return np.array(alignments), mutation_types
+    
+
+    def char_to_int(self, alignments=None):
+        """Maps the char ACGT to the corresponding integers"""
+        if alignments is None:
+            alignments = self.alignments
+        return np.vectorize(self.transdict.get)(alignments)
+    
+
+    def plot_data(self, alignments_ints=None, mutation_types=None, mutation_index=0):
+        """Use to plot an alignment at a certain mutation type"""
+        if alignments_ints is None or mutation_types is None:
+            alignments_ints = self.char_to_int()
+            mutation_types = self.mutation_types
+        
+        plt.rcParams['figure.dpi'] = 200
+        alignment_idx = mutation_types.index(mutation_index)
+        plt.title(f"Mutation type: {self.mutation_type_names[mutation_types[alignment_idx]]}")
+        plt.imshow(alignments_ints[alignment_idx],cmap='jet')
