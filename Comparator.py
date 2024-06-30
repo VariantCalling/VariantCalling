61f07fa85e662cc7ac7497852087bf64d78a73db
Marcus Lim <marcuslimguozong@gmail.com>
2024-06-30 22:50:17 +0800

    Adding model-loading, truncation, and inference-running wrapper for comparator

--- Comparator.py
+++ Comparator.py
@@ -43,3 +43,66 @@ class Transformer(keras.layers.Layer):
         ffn_output = self.ffn_dropout(ffn_output, training=training)
         ffn_output = self.ffn_norm(attention_output + ffn_output)
         return ffn_output
+
+def load_comparator(model_path):
+    """
+    Wrapper to load the comparator model with self-handled initialization of custom Transformer model
+    """
+    return tf.keras.models.load_model(model_path, custom_objects={"Transformer": Comparator.Transformer},safe_mode=False)
+
+def truncate_seq(truncation_range,seq_in):
+    truncated_chunks = []
+    for truncation_chunk in truncation_range:
+        truncated_chunks.append(seq_in[:, truncation_chunk[0]:truncation_chunk[1]])
+    for i in range(len(truncated_chunks)):
+        print(i)
+        if i == 0:
+            seq_in = truncated_chunks[i]
+            print(np.shape(np.array(seq_in)))
+        else:
+            seq_in = np.concatenate((seq_in, truncated_chunks[i]), axis=1)
+            print(np.shape(np.array(seq_in)))
+    return seq_in
+
+def run_comparator_inference(model, pickle_file, truncate_range=[]):
+    """
+    Wrapper to run inference using comparator model and a user-input pickle file
+    """
+    pickle_file = pickle_file
+    pickle_seq = []
+
+    with (open(pickle_file, "rb")) as openfile:
+        while True:
+            try:
+                pickle_seq.append(pickle.load(openfile))
+            except EOFError:
+                break
+    X_inp_char = []
+    for element in pickle_seq[0]:
+        X_inp_char.append(list(element[2]))
+    X_inp_char = np.array(X_inp_char)    
+
+    transdict = {"A":0, "C": 1, "G":2, "T":3}
+    X_inp_val = np.vectorize(transdict.get)(X_inp_char)
+
+    if truncate_range != []:
+        X_inp_val = truncate_seq(X_inp_valr)
+
+    clones = []
+    with open("clones_lib/" + clone_file, "r") as f:
+        for clone in f:
+            alignment = []
+            for char in clone.strip():
+                alignment.append(char)
+            clones.append(alignment)
+    proportion_tracker = [0 for i in len(clones) + 1]
+    clones_int = np.vectorize(transdict.get)(clones)
+    pred_list = []
+    for i in range(len(clones_int)):
+        ref_list = []
+        inp_list = []
+        for j in range(np.shape(X_inp_val)[0]):
+            ref_list.append(clones_int[i])
+            inp_list.append(X_inp_val[j].tolist())
+        pred_list.append(model.predict([pd.DataFrame(ref_list).values, pd.DataFrame(inp_list).values]))
+    return pred_list
